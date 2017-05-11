0108e9f7000376a3864ac9923c96fe9d5c3751f2
Christan Plaza <Christan Plaza>
2017-05-11 16:23:00 +0800

    Initialization, Firebase Config, Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { enableProdMode } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AngularFireModule } from 'angularfire2';
+import { firebaseConfig } from './../environments/firebase.config';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DiscountsComponent } from './components/discounts/discounts.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DiscountComponent } from './components/discount/discount.component';
+import { AddDiscountComponent } from './components/add-discount/add-discount.component';
+import { EditDiscountComponent } from './components/edit-discount/edit-discount.component';
 
+const appRoutes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'discounts',component:DiscountsComponent}
+]
+
+enableProdMode();
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DiscountsComponent,
+    NavbarComponent,
+    DiscountComponent,
+    AddDiscountComponent,
+    EditDiscountComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBSNSbaUW7xzy1rTRY7jTOU7zLZ369VhbI",
+    authDomain: "kazzhing-9f1d1.firebaseapp.com",
+    databaseURL: "https://kazzhing-9f1d1.firebaseio.com",
+    storageBucket: "kazzhing-9f1d1.appspot.com"
+}
